453e88444b3bc7f92b22d589a51354c3252c0a6f
milhousecvh <135559562+milhousecvh@users.noreply.github.com>
2023-11-02 13:16:26 -0500

    everything so far i can figure out

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
@@ -28,17 +29,21 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @GetMapping(value = "results")
-    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchValue) {
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-            jobs = JobData.findByColumnAndValue(searchType, searchValue);
-            model.addAttribute("type", searchType);
-            model.addAttribute("columns", columnChoices);
-            model.addAttribute("title", "Results for search of :" + columnChoices.get(searchType) + " : " + searchValue);
-            model.addAttribute("jobs", jobs);
-        return "search.html";
+        if (searchTerm.equals("all")) {
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("type", searchType);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Results for search of :" + columnChoices.get(searchType) + " : " + searchTerm);
+        model.addAttribute("jobs", jobs);
+        return "search";
     }
-
 
 
 }
